3bd1f1ba342505bd6ab07367d02b0588f7f80a42
Steffen Remus <steremus@gmail.com>
2019-01-11 14:37:51 +0100

    py bind fasttext

---
+++ R/embedding.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: remstef
+"""
+
+from pyfasttext import FastText
+
+class FastTextEmbedding(Embedding):
+
+  def __init__(self, binfile, normalize = False):
+    self.file = binfile
+    self.vdim = -1
+    self.normalize = normalize
+
+  def load(self):
+    print('Loading fasttext model.')
+    self.ftmodel = FastText()
+    self.ftmodel.load_model(self.file)
+    self.vdim = len(self.ftmodel['is'])
+    print('Finished loading fasttext model.')
+    return self
+
+  def getVector(self, word):
+    return self.ftmodel.get_numpy_vector(word, normalized = self.normalize)
+
+  def wordForVec(self, v):
+    word, sim = self.ftmodel.words_for_vector(v)[0]
+    return word, sim
+
+  def nearest_neighbors(self, word, n=200):
+    tuples = ftmodel.nearest_neighbors(word, n)
+    return tuples;
+
+  def containsWord(self, word, explicit=False):
+    if explicit:
+      return word in vocabulary()
+    return True
+
+  def vocabulary(self):
+    return self.ftmodel.words
+
+  def dim(self):
+    return self.vdim
--- README.md
+++ README.md
@@ -4,5 +4,13 @@ linux users use aliases, e.g.:
 
 ` alias sensevectors='Rscript --vanilla --default-packages=methods,utils,stats bin/sensevectors' `
 
+create an R environment file which contains the following content:
 
-
+``
+DATA_HOME='~/data'
+DATA_TEMP='~/data/temp'
+WNHOME='~/data/wordnet/WordNet-3.0'
+MAILGUN_API='https://api:key-xyz@api.mailgun.net/v3/zyx.mailgun.org/messages'
+MAILGUN_RECEPIENT='name@server.com'
+RETICULATE_PYTHON='/usr/local/miniconda3/bin/python3'
+``
