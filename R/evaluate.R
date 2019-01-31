bbf4063057250d71f098613c042419d99d735b07
Steffen Remus <steremus@gmail.com>
2019-01-31 23:14:47 +0100

    ft

--- R/evaluate.R
+++ R/evaluate.R
@@ -320,6 +320,11 @@ with(evaluate, {
     )
   }
 
+  .try.run.fteval <- function(par = NULL, evalfilter = function(e) F, only_best_inventory=F){
+    ftfilter < function(e) is.na(e$vsmodel) || grep('_ft_cc_', e$vsmodel) || evalfilter(e)
+    evaluate$.try.run.eval(pat, ftfilter, only_best_inventory)
+  }
+
   .get.results <- function(results){
     #
     if(is.data.frame(results)){
